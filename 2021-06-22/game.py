5eb0dff2a41a168ab39970d2589ca2b96685698f
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-06-29 19:06:56 -0300

    Solucao para Jogo da Vida = 42

--- 2021-06-22/game.py
+++ 2021-06-22/game.py
@@ -1,38 +1,52 @@
 def run_turn(cells):
-    response = []
+    next_turn_cells = []
+    neighborhood = []
     for x, y in cells:
         count = 0
-        if (x-1, y) in cells:
+        neighborhood.append((x-1, y))
+        neighborhood.append((x+1, y))
+        neighborhood.append((x, y-1))
+        neighborhood.append((x, y+1))
+        neighborhood.append((x-1, y-1))
+        neighborhood.append((x-1, y+1))
+        neighborhood.append((x+1, y+1))
+        neighborhood.append((x+1, y-1))
+        if (x-1, y) in cells: # a esquerda
             count += 1
-        if (x+1, y) in cells:
+        if (x+1, y) in cells: # a direita
             count += 1
-        if (x, y-1) in cells:
+        if (x, y-1) in cells: # abaixo
             count += 1
-        if (x, y+1) in cells:
+        if (x, y+1) in cells: # acima
             count += 1
-        if (x-1, y-1) in cells:
+        if (x-1, y-1) in cells: # baixa esquerda
             count += 1
-        if (x+1, y+1) in cells:
+        if (x-1, y+1) in cells: # cima esquerda
+            count += 1
+        if (x+1, y+1) in cells: # cima direita
+            count += 1
+        if (x+1, y-1) in cells: # baixa direita
             count += 1
 
         if count < 2 or count > 3:
-            "MORRE DESGRACADO!"
-        if count = 2 or count
+            pass
+        if count == 3 or count == 2:
+            next_turn_cells.append((x, y))
 
-vizinhanca = [(0, 1), (1, 0), (1, 1), (0, 1), (1, 1), (1, 2), (2, 1), (1, 1), (1, 2)]
+    slots = set(neighborhood)
+    for slot in slots:
+        if neighborhood.count(slot) == 3 and slot not in next_turn_cells:
+            next_turn_cells.append(slot)
 
-
+    return sorted(next_turn_cells)
 
 if __name__ == "__main__":
-    cells = [(0, 0), (0, 2), (2, 2)]
+    assert run_turn([(0, 0), (0, 2), (2, 2)]) == [(1, 1)]
+    assert run_turn([(1, 1)]) == []
+    assert run_turn([(0, 1), (1, 0), (1, 1), (1, 2)]) == [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 1)]
+    assert run_turn([(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 1)]) == [(-1, 1), (0, 0), (0, 2), (2, 0), (2, 1), (2, 2)]
 
-    turn = run_turn(cells)
-    assert turn == [(0, 1), (0, 2)]
-
-
-
-# board = [
-#     [Cell(0, 0), Cell(0, 1), Cell(0, 2)],
-#     [Cell(1, 0), Cell(1, 1), Cell(1, 2)],
-#     [Cell(2, 0), Cell(2, 1), Cell(2, 2)],
-# ]
+    result = [(0, 1), (1, 1), (1, 2), (2, 1), (2, 2), (3, 2)]
+    for turn in range(10):
+        result = run_turn(result)
+        print(f"Turno: {turn+1} = {result}")
